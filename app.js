727c5c1eb9f56bff64b534d2db46d1ffdf2d967d
Srishti Singh <srishtisingh15feb@gmail.com>
2021-07-04 11:14:40 +0530

    Final changes

--- app.js
+++ app.js
@@ -2,8 +2,25 @@ var btnTranslate =document.querySelector("#btn-translate");
 var txtInput=document.querySelector("#txt-input");
 var outputDiv=document.querySelector("#output");
 
+//var serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL="https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text
+}
+
 
 function clickHandler(){
-    outputDiv.innerText = "ajsjsjsjs " + txtInput.value;
+    var inputText= txtInput.value //input
+
+    //calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+        var translatedText=json.contents.translated;
+        outputDiv.innerText = translatedText;
+        
+        })
 };
+
 btnTranslate.addEventListener("click", clickHandler)
